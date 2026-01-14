e8397ad1f98444a6962c6ee5c9d9115b47d239e7
davidEmento <david@emento.dkm>
2026-01-14 14:41:20 +0100

    Initial commit

---
+++ GladargMedia.lua
@@ -0,0 +1,36 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+LSM:Register("background", "arrow_glow", [[Interface\AddOns\SharedMedia_Gladarg\background\arrow_glow.tga]])
+LSM:Register("background", "melli", [[Interface\AddOns\SharedMedia_Gladarg\background\melli.tga]])
+LSM:Register("background", "EthricArrow", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow.tga]])
+LSM:Register("background", "EthricArrow2", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow2.tga]])
+LSM:Register("background", "EthricArrow3", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow3.tga]])
+LSM:Register("background", "EthricArrow4", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow4.tga]])
+LSM:Register("background", "EthricArrow5", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow5.tga]])
+LSM:Register("background", "EthricArrow6", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow6.tga]])
+LSM:Register("background", "EthricArrow7", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow7.tga]])
+LSM:Register("background", "EthricArrow8", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow8.tga]])
+LSM:Register("background", "EthricArrow9", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow9.tga]])
+LSM:Register("background", "EthricArrow10", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow10.tga]])
+LSM:Register("background", "NaowhArrow", [[Interface\AddOns\SharedMedia_Gladarg\background\NaowhArrow.tga]])
+LSM:Register("background", "NaowhArrowLeft", [[Interface\AddOns\SharedMedia_Gladarg\background\NaowhArrowLeft.tga]])
+LSM:Register("background", "NaowhArrowRight", [[Interface\AddOns\SharedMedia_Gladarg\background\NaowhArrowRight.tga]])
+LSM:Register("background", "NaowhGlow", [[Interface\AddOns\SharedMedia_Gladarg\background\NaowhGlow.tga]])
+
+-- -----
+--  BORDER
+-- ----
+LSM:Register("border", "Naowh1", [[Interface\AddOns\SharedMedia_Gladarg\border\Naowh1.tga]])
+LSM:Register("border", "Naowh2", [[Interface\AddOns\SharedMedia_Gladarg\border\Naowh2.tga]])
+LSM:Register("border", "Naowh3", [[Interface\AddOns\SharedMedia_Gladarg\border\Naowh3.tga]])
+LSM:Register("border", "roth", [[Interface\AddOns\SharedMedia_Gladarg\border\roth.tga]])
+LSM:Register("border", "SeerahScalloped", [[Interface\AddOns\SharedMedia_Gladarg\border\SeerahScalloped.tga]])
+
+-- -----
+--  FONT
+-- -----
+LSM:Register("font", "Naowh", [[Interface\Addons\SharedMedia_Gladarg\font\Naowh.ttf]]) 
+LSM:Register("font", "Gotham Narrow Ultra", [[Interface\Addons\SharedMedia_Gladarg\font\GothamNarrowUltra.ttf]])
