456872afb7f7de3252660df650c4b283ace272d1
davidEmento <david@emento.dkm>
2026-01-26 11:42:26 +0100

    Initial commit

---
+++ GladargMedia.lua
@@ -0,0 +1,35 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+LSM:Register("background", "melli", [[Interface\AddOns\SharedMedia_Gladarg\background\melli.tga]])
+LSM:Register("background", "EthricArrow", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow.tga]])
+LSM:Register("background", "EthricArrow2", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow2.tga]])
+LSM:Register("background", "EthricArrow3", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow3.tga]])
+LSM:Register("background", "EthricArrow4", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow4.tga]])
+LSM:Register("background", "EthricArrow5", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow5.tga]])
+LSM:Register("background", "EthricArrow6", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow6.tga]])
+LSM:Register("background", "EthricArrow7", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow7.tga]])
+LSM:Register("background", "EthricArrow8", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow8.tga]])
+LSM:Register("background", "EthricArrow9", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow9.tga]])
+LSM:Register("background", "EthricArrow10", [[Interface\AddOns\SharedMedia_Gladarg\background\EthricArrow10.tga]])
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--  FONT
+-- -----
+LSM:Register("font", "Questrial", [[Interface\Addons\SharedMedia_Gladarg\font\Questrial-Regular.ttf]])
+LSM:Register("font", "Inter_28pt-Medium", [[Interface\Addons\SharedMedia_Gladarg\font\Inter_28pt-Medium.ttf]])
+LSM:Register("font", "Inter_18pt-Medium", [[Interface\Addons\SharedMedia_Gladarg\font\Inter_18pt-Medium.ttf]])
+LSM:Register("font", "Gladarg", [[Interface\Addons\SharedMedia_Gladarg\font\Gladarg.ttf]])
+
+
+-- -----
+--  STATUSBAR
+-- -----
+LSM:Register("statusbar", "Gladarg Blank", [[Interface\Addons\SharedMedia_Gladarg\statusbar\GladargBlank.tga]])
+LSM:Register("statusbar", "Gladarg Cast", [[Interface\Addons\SharedMedia_Gladarg\statusbar\GladargCast.tga]])
---
+++ HOWTO.md
@@ -0,0 +1,16 @@
+# GladargUI: Update Cheat Sheet
+
+Follow these steps whenever you want to push a new version to CurseForge.
+
+### 1. Update the Code
+- **Crucial:** Update `## Version: X.X.X` in `SharedMedia_Gladarg.toc`.
+
+### 2. Run the Update Batch
+Paste this into the VS Code terminal (replace `v1.x.x` with your new version):
+
+```bash
+git add .
+git commit -m "Updated strings"
+git tag v1.0.15
+git push origin main
+git push origin v1.0.15
